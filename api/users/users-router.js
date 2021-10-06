8a7e28e4ba48562f403fe503878eaeef23ffdaea
Marina Monsivais <minamonsi@me.com>
2021-10-05 21:18:36 -0500

    WIP

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -18,38 +18,51 @@ router.get('/', (req, res, next) => {
    .catch(next)
 });
 
-router.get('/:id', validateUserId, (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+router.get('/:id', validateUserId, (req, res, next) => {
+  res.json(req.user)
 });
 
-router.post('/', (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+router.post('/', validateUserId,(req, res) => {
+Users.insert({ name: req.name })
+.then(newUser =>{
+  res.status(201).json(newUser)
+})
+.catch(next)
 });
 
-router.put('/:id', (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.put('/:id', validateUserId,validateUser,(req, res, next) => {
+  Users.update(req.params.id, { name: req.name })
+  .then(() => {
+    return Users.getById(req.params.id)
+  })
+  .then(user => {
+    res.json(user)
+  })
+  .catch(next)
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId,(req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
+//error handling router
+router.use((err,req,res,next)=>{
+  res.status(500).json({
+    message: err.message
+  })
+})
 // do not forget to export the router
 
 module.exports = router
